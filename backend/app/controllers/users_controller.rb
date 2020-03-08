91f7f59b32a5b00b26e476219c9fe62b70f345cb
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-03-08 03:43:38 -0400

    users database created, api end points are working

---
+++ backend/app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  before_action :set_users, only: [:show, :update]
+
+  def show
+    render json: @user
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+      user = @user
+      token = JWT.encode({ user_id: @user.id }, ENV["JWT_SECRET"], "HS256")
+      render json: { user: user, token: token }
+    else
+      render json: { errors: user.errors.full_messages }
+    end
+  end
+
+  def update
+    @user.update(user_params)
+    render json: @user
+  end
+
+  private
+
+  def set_users
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:name, :email, :password)
+  end
+end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "http"
+require "json"
+
+User.destroy_all
+
+def search(tickers, type, api_key)
+  url = "https://cloud.iexapis.com/stable/stock/market/batch/"
+  params = {
+    symbols: tickers,
+    types: type,
+    token: api_key,
+  }
+  response = HTTP.get(url, params: params)
+  # .auth("Bearer #{ENV["API_TOKEN"]}").
+  json_response = response.parse
+  puts json_response
+end
+
+search("aapl, fb, msft, googl, amzn, snap, uber, tsla", "quote", ENV["API_TOKEN"])
