7bbeaf3f094bfab562c83e6114926333dcfed4c6
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-03-08 17:03:15 -0400

    populated stocks database from api request, serializers working, model relationships working, backend complete

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/auth", to: "auth#persist"
   post "/login", to: "auth#login"
   resources :users, only: [:show, :create, :update]
+  resources :stocks, only: [:index, :show]
   resources :transactions, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -9,6 +9,7 @@
 require "http"
 require "json"
 
+Stock.destroy_all
 User.destroy_all
 
 def search(tickers, type, api_key)
@@ -21,7 +22,19 @@ def search(tickers, type, api_key)
   response = HTTP.get(url, params: params)
   # .auth("Bearer #{ENV["API_TOKEN"]}").
   json_response = response.parse
-  puts json_response
+  json_response
 end
 
-search("aapl, fb, msft, googl, amzn, snap, uber, tsla", "quote", ENV["API_TOKEN"])
+iex_stock = search("aapl, fb, msft, googl, amzn, snap, uber, tsla", "quote", ENV["API_TOKEN"])
+
+def create_stocks(response)
+  response.each do |company, stock_info|
+    Stock.create(
+      ticker: stock_info["quote"]["symbol"],
+      open_price: stock_info["quote"]["open"],
+      current_price: stock_info["quote"]["latestPrice"],
+    )
+  end
+end
+
+create_stocks(iex_stock)
